03c37c7e866ef831e3f3e9c89b7a7b42bfd91d14
Alek2750 <alek2750@gmail.com>
2019-01-31 16:25:25 +0100

    projekt

---
+++ MavenGetStarted/src/main/java/com/mycompany/mavengetstarted/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavengetstarted;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
